5be022e13b2da5ee879bc173b1defb1a3f6bb4b9
Ian Campos <62730043+IanCamposDeAssis@users.noreply.github.com>
2024-10-18 21:04:38 -0300

    2ºreboot

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="playfair.css">
+    <link rel="stylesheet" href="style/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Playfair:ital,opsz,wght@0,5..1200,300..900;1,5..1200,300..900&display=swap"
+        rel="stylesheet">
+    <title>Vinhos.net</title>
+</head>
+
+<body>
+    <header class="menu" id="inicio"> 
+        <!-- menu de navegação -->
+        <nav>
+            <ul>
+                <li><a href="#inicio">Início</a></li>
+                <li><a href="#sobre">Sobre</a></li>
+                <li><a href="#contato">Contato</a></li>
+            </ul>
+        </nav>
+        <!-- menu do usuário -->
+        <div class="user-area">
+            <!-- foto de usuário -->
+            <div class="user-photo">
+                <img src="assets/users/default.png" alt="Foto de usuário">
+            </div>
+            <!-- opções do usuário -->
+            <div class="user-options">
+                <ul>
+                    <li>Mais opções</li>
+                    <ul>
+                        <li>Logar / Minha conta</li>
+                        <li>Sair</li>
+                    </ul>
+                </ul>
+
+
+
+
+
+
+            </div>
+        </div>
+    </header>
+    <main class="conteudo" id="sobre">
+        <div class="fundo">
+            <section class="destaque" id="destaque">
+                <div class="cards" id="cards">
+                    <div class="item ">
+                        <div class="imagem-card">
+                            <img src="assets/01.png" alt="rose">
+                        </div>
+                        <div class="descricao-card">
+                            <h2>Preto</h2>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="imagem-card">
+                        <img src="assets/01.png" alt="tinto">
+                        </div>
+                        <div class="descricao-card">
+                        <h2>Vermelho</h2>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="imagem-card">
+                        <img src="assets/01.png" alt="branco">
+                        </div>
+                        <div class="descricao-card">
+                        <h2>Azul</h2>
+                    </div>
+                    </div>
+                </div>
+        </div>
+        </section>
+
+        <section class="produtos-gerais">
+            ss
+        </section>
+    </main>
+    <footer class="rodape" id="contato">
+        hhh
+    </footer>
+</body>
+
+</html>
